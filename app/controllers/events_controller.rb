c14a8135ea6a584276db81fbfee76d06c79b930a
Henrique Barros <henriquexavierb@gmail.com>
2021-02-05 16:35:25 -0300

    including user email into event query data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
-    render json: @events
+    render json: @events, :include => { :user => { :only => [:email] }}
   end
 
   def show
     @event = Event.find(params[:id])
-    render json: @event
+    render json: @event, :include => { :user => { :only => [:email] }}
   end
 
   def create
