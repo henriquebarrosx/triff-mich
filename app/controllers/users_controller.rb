829c8f45cda454f28d5349803c073977e31ed606
Henrique Barros <henriquexavierb@gmail.com>
2021-02-05 12:22:33 -0300

    creating user CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: { message: "Formato de envio para cadastro de usuário inválido" }
+    end 
+  end
+
+  def put
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render json: @user
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end  
+end
