7af688a4ed7b7f8803125d11fda213f385b9ad84
Henrique Barros <henriquebarrosx@gmail.com>
2021-02-08 11:32:52 -0300

    fixing fetch events by user id

--- app/services/events/fetch.rb
+++ app/services/events/fetch.rb
@@ -1,21 +1,25 @@
 module Events
   class Fetch
     def self.call(event_params)
-      events = Event.all.order(created_at: :desc)
+      events = Event.all
 
       if event_params[:theme]
-        events = Event.where("theme ILIKE ?", "%#{event_params[:theme]}%").order(created_at: :desc)
+        events = events.where("theme ILIKE ?", "%#{event_params[:theme]}%")
       end
 
       if event_params[:is_favorite]
-        events = Event.where({ :is_favorite => event_params[:is_favorite] }).order(created_at: :desc)
+        events = events.where({ :is_favorite => event_params[:is_favorite] })
       end
 
       if event_params[:order_by]
-        events = Event.all.order(start_time: event_params[:order_by])
+        events = events.order(start_time: event_params[:order_by])
       end
 
-      events
+      if event_params[:user_id]
+        events = events.where(:user_id => event_params[:user_id])
+      end
+
+      events = events.order(created_at: :desc)
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_154106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "accounts", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.text "expired_token"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
-  end
-
   create_table "events", force: :cascade do |t|
     t.string "event_representative"
     t.datetime "start_time"
