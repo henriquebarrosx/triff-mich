af6c43554d9775f19d51964452840554466a8c02
Henrique Barros <henriquebarrosx@gmail.com>
2021-02-10 00:22:23 -0300

    set default value into migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
 class Event < ApplicationRecord
   belongs_to :user
-  validates_presence_of :theme, :event_representative, :start_time, :end_time, :link, :description
 end
--- db/migrate/20210208161853_create_events.rb
+++ db/migrate/20210208161853_create_events.rb
@@ -1,16 +1,16 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
+      t.string :theme
       t.string :event_representative
       t.datetime :start_time
       t.datetime :end_time
-      t.text :theme
       t.text :link
-      t.boolean :is_available, default: false
-      t.boolean :is_favorite, default: false
+      t.boolean :is_expired
+      t.boolean :is_available
+      t.boolean :is_favorite
       t.text :description
-      t.boolean :is_expired, default: false
-      t.references :user, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
