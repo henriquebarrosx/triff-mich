60fa15d28b6f9fad64b676b9497a9c252db31e3e
Henrique Barros <henriquebarrosx@gmail.com>
2021-02-10 00:19:22 -0300

    replacing common migration to devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_154106) do
+ActiveRecord::Schema.define(version: 2021_02_08_161853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
+    t.string "theme"
     t.string "event_representative"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.text "theme"
     t.text "link"
-    t.boolean "is_available", default: false
-    t.boolean "is_favorite", default: false
+    t.boolean "is_expired"
+    t.boolean "is_available"
+    t.boolean "is_favorite"
     t.text "description"
-    t.boolean "is_expired", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -32,11 +32,16 @@ ActiveRecord::Schema.define(version: 2021_02_05_154106) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "name"
-    t.string "email"
-    t.text "expired_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "events", "users"
